78010c8ba4184ac9f47a788d78aff0947a9ac213
ritesh6916 <riteshsingh0500@gmail.com>
2023-07-06 01:31:16 +0530

    welcome controller added

--- src/main/java/com/springmvc/interceptor/controllers/HomeController.java
+++ src/main/java/com/springmvc/interceptor/controllers/HomeController.java
@@ -1,5 +1,23 @@
 package com.springmvc.interceptor.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
 public class HomeController {
 
+	@RequestMapping("/")
+	public String homeController()
+	{
+		return "Home";
+	}
+	
+	@RequestMapping("welcome")
+	public String welcomeController(@RequestParam("txtname") String name, Model m)
+	{
+		m.addAttribute("name", name);
+		return "Welcome";
+	}
 }
