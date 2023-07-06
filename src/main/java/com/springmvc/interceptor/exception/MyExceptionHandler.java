2674826da8ad68c13f744e97580e0016f094e914
ritesh6916 <riteshsingh0500@gmail.com>
2023-07-06 12:24:25 +0530

    MyBadluckException handled

--- src/main/java/com/springmvc/interceptor/exception/MyExceptionHandler.java
+++ src/main/java/com/springmvc/interceptor/exception/MyExceptionHandler.java
@@ -1,5 +1,23 @@
 package com.springmvc.interceptor.exception;
 
-public class MyExceptionHandler {
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@ControllerAdvice
+public class MyExceptionHandler {
+	
+	@ExceptionHandler(value = MyBadluckException.class)
+	public String badluckHandler(Model m )
+	{
+		m.addAttribute("msg","You are unlucky please try again...");
+		return "customErrorPage";
+	}
+	
+	@ExceptionHandler(NullPointerException.class)
+	public String nullPointerhandler(Model m)
+	{
+		m.addAttribute("msg","NullPointerExceptionOccured...");
+		return "customErrorPage";
+	}
 }
